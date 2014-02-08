4ff7887059687bcac06143b9b8bec426f5a1e3c9
Xu Hao <xuhao3e8@gmail.com>
2014-02-08 18:20:56 +0800

    Building GUI

--- src/display.cpp
+++ src/display.cpp
@@ -7,7 +7,6 @@
 
 // 太阳、地球和月亮
 // 假设每个月都是 30 天 //一年12个月,共是360天
-long lasttime=0;
 long getCurrentTime()  
 {  
 	struct timeval tv;  
@@ -17,46 +16,37 @@ long getCurrentTime()
 // 太阳、地球和月亮
 // 假设每个月都是 12 天 //一年12个月,共是360天
 static int day = 200; // day 的变化:从 0 到 359
-
+double count=-0.5;
 void myDisplay(void) 
 {
-	glEnable(GL_DEPTH_TEST);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(75, 1, 1, 400000000); 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, -200000000, 200000000, 0, 0, 0, 0, 0, 1);
-	// 绘制红色的“太阳”
-	glColor3f(1.0f, 0.0f, 0.0f); 
-	glutSolidSphere(69600000, 20, 20); // 绘制蓝色的“地球”
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glRotatef(day/360.0*360.0, 0.0f, 0.0f, -1.0f);
-	glTranslatef(150000000, 0.0f, 0.0f); 
-	glutSolidSphere(15945000, 20, 20);
-	// 绘制黄色的“月亮”
-	glColor3f(1.0f, 1.0f, 0.0f);
-	glRotatef(day/30.0*360.0 - day/360.0*360.0, 0.0f, 0.0f, -1.0f); 
-	glTranslatef(38000000, 0.0f, 0.0f);
-	glutSolidSphere(4345000, 20, 20);
+	//glColor3f(1.0,0,0);
+	count+=0.01;
+	if(count>0.5)
+		count=-0.5;
+	glRectf(-0.5f+count, -0.5f, 0.5f+count, 0.5f);
+
+	glBegin(GL_LINES);
+		glVertex2i(180,15);
+		glVertex2i(10,145);
+	glEnd();
+
 	glFlush();
-	glutSwapBuffers(); 
 }
+long lasttime=0;
 void myIdle(void) 
 {
-	++day;
-	if( day >= 360 )
-		day = 0; 
-	myDisplay();
+	long now=getCurrentTime();
+	if(now-lasttime>1000)
+		myDisplay();
 }
 int main(int argc, char *argv[]) 
 {
 	glutInit(&argc, argv); 
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowPosition(500, 100);
 	glutInitWindowSize(400, 400);
-	glutCreateWindow("太阳,地球和月亮");
+	glutCreateWindow("HighWay");
 	glutDisplayFunc(&myDisplay); 
 	glutIdleFunc(&myIdle);
 	glutMainLoop();
